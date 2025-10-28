2f8477baefae72244673c3a01f5f25fca556dede
Jose Trinidad Lopez <jtlopez@pinnacleaerospace.com>
2025-10-28 14:19:19 -0700

    Documentation RCC driver header

--- GRID_FORMING/drivers/Inc/stm32f446xx_rcc_driver.h
+++ GRID_FORMING/drivers/Inc/stm32f446xx_rcc_driver.h
@@ -10,7 +10,9 @@
 
 #include "stm32f446xx.h"
 
-uint32_t RCC_GetPClk1(void);
-uint32_t RCC_GetPClk2(void);
+/*Global Clock Variables*/
+
+uint32_t RCC_GetPClk1(void); // Peripheral Clock 1 Variable = APB1 Clock
+uint32_t RCC_GetPClk2(void); // Peripheral Clock 2 Variable = APB2 Clock
 
 #endif /* INC_STM32F446XX_RCC_DRIVER_H_ */
