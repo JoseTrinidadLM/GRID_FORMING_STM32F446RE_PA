b9fb5999bb2a2d476c5d803c057b83e2e098e577
Jose Trinidad Lopez <jtlopez@pinnacleaerospace.com>
2025-11-26 15:42:32 -0700

    TestFrameWork folder & batch script Automated Debugger Connection

---
+++ TestFrameWork/debug_stm32.bat
@@ -0,0 +1,17 @@
+
+@echo off
+wt ^
+    new-tab --title "ST-LINK GDB Server" cmd /k ^
+        C:\ST\STM32CubeCLT_1.20.0\STLink-gdb-server\bin\ST-LINK_gdbserver.exe ^
+        -cp C:\ST\STM32CubeCLT_1.20.0\STM32CubeProgrammer\bin ^
+        -i 0669FF545383564867214231 ^
+        --halt --frequency 4000 ^
+        -d ^
+    ; new-tab --title "GDB Debug" cmd /k ^
+        arm-none-eabi-gdb ^
+        C:\Users\jtlopez\Documents\InternProject\STM32F446RE\GRID_FORMING\Debug\GRID_FORMING.elf ^
+        -ex "target remote localhost:61234" ^
+        -ex "monitor halt" ^
+        -ex "load" ^
+        -ex "break main" ^
+        -ex "continue"
